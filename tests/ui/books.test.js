b235f450f05ddd92a63aed73f67b2fa4a071c707
Murtada Ahmed <murtadhabg@gmail.com>
2024-02-24 02:17:04 +0200

    added ui test

--- tests/ui/books.test.js
+++ tests/ui/books.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check books page', async ({ page }) => {
-    await page.goto('http://localhost:8080/books');
+    await page.goto('https://murtadoc-exam-prep-07-12-2023.onrender.com/books');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
